8b25cf659db84f1b7db8e4f4452eeb4723b8db67
tahaabbaszaidi <53249909+tahaabbaszaidi@users.noreply.github.com>
2022-11-07 17:11:12 +0500

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ var models = [
     },
     {
         url: './assets/dragonite/scene.gltf',
-        scale: '0.9 0.9 0.9',
+        scale: '0.01 0.01 0.01',
         rotation: '0 180 0',
         info: 'Dragonite, Lv. 99, HP 150/150',
     },
