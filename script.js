12aa677a00ad439da698da7d08d16efe7bae7111
tahaabbaszaidi <53249909+tahaabbaszaidi@users.noreply.github.com>
2022-11-07 18:40:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 function loadPlaces(position) {
     const params = {
         radius: 300,    // search places not farther than this value (in meters)
-        clientId: CRVPHYM4OKKKGXXW45NMTUJMPHOEJSGGGDDSPRH0B1UOOIKZ,
-        clientSecret: A1H3HP3ETAHXNFFMBISPLY3NSUTL3OKYBIZLCRSVMN3V3EEF,
+        clientId: 'CRVPHYM4OKKKGXXW45NMTUJMPHOEJSGGGDDSPRH0B1UOOIKZ',
+        clientSecret: 'A1H3HP3ETAHXNFFMBISPLY3NSUTL3OKYBIZLCRSVMN3V3EEF',
         version: '20300101',    // foursquare versioning, required but unuseful for this demo
     };
 
